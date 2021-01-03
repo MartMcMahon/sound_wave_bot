31a160b1a33487e990b3884d4bc9886f4f2bd130
dwieb <dwayne.harmon@gmail.com>
2021-01-03 01:39:46 -0600

    bot.js -> server.js

--- test.js
+++ test.js
@@ -1,9 +1,14 @@
 const axios = require('axios')
 
 const url = "https://accounts.spotify.com/authorize"
+const url2 = "https://accounts.spotify.com/api/token/"
 const client_id = "e045f41542f9475e99b142f8359bc368"
 
-axios.get(url + `/?client_id=${client_id}&response_type=code&redirect_uri=https://mart.pizza`).then(res=>{
+// axios.get(url + `/?client_id=${client_id}&response_type=code&redirect_uri=https://mart.pizza`).then(res=>{
+//   console.log(res)
+//   return res
+// })
+axios.get(url2 + `authorization_code=AQB7a7as51HoIbHoihcZjlJ1tM9oPB_RbCh2DXl5Qz7xGo8EhgPm1AdFgijQcSGSXTdJjIZOG_wdy - y6 - 3ktBagE0NU535w_GMplZy2ZfLmHp_kG7ZFJucWeiEFpx_0veC27iHLjWqfFAyqLvqawE1x67yfBnw&client_secret=f9a017e91e5741448e310db917580dbf`).then(res=>{
   console.log(res)
   return res
 })
