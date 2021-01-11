f8c468a12a0e64006502d5717a9a42c6067d2df5
Mart <themcman@gmail.com>
2021-01-11 00:50:17 -0600

    lets gooo

--- server.js
+++ server.js
@@ -1,88 +1,76 @@
-const tmi = require("tmi.js");
+const axios = require("axios");
+const querystring = require("querystring");
 
-// Define configuration options
-const tmi_opts = {
-  identity: {
-    username: process.env.BOT_USERNAME,
-    password: process.env.OAUTH_TOKEN,
-  },
-  channels: [process.env.CHANNEL_NAME],
-};
+const express = require("express");
+const app = express();
+const port = 80;
 
-// Create a client with our options
-const client = new tmi.client(tmi_opts);
+const client_id = process.env.SPOTIFY_CLIENT_ID;
+const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+const redirect_uri = process.env.REDIRECT_URI;
 
-// Register our event handlers (defined below)
-client.on("message", onMessageHandler);
-client.on("connected", onConnectedHandler);
+let code = null;
+let token = "";
+let refresh_token = "";
+let token_exp = 0;
 
-// Connect to Twitch:
-client.connect();
+app.get("/", (req, res) => {
+  const token_url = "https://accounts.spotify.com/api/token";
+  code = req.query.code;
 
-// Called every time a message comes in
-function onMessageHandler(target, context, msg, self) {
-  if (self) {
-    return;
-  } // Ignore messages from the bot
+  console.log("code is ", code);
+  console.log(
+    "auth is ",
+    Buffer.from(`${client_id}:${client_secret}`).toString("base64")
+  );
+  axios({
+    method: "post",
+    url: token_url,
+    data: querystring.stringify({
+      grant_type: "authorization_code",
+      code,
+      redirect_uri: redirect_uri,
+    }),
+    headers: {
+      Authorization:
+        "Basic " +
+        Buffer.from(`${client_id}:${client_secret}`).toString("base64"),
+      "content-type": "application/x-www-form-urlencoded;charset=utf-8",
+    },
+  })
+    .then((token_response) => {
+      token = token_response.data.access_token;
+      token_exp = Date.now() + token_response.expires_in * 1000;
+      refresh_token = token_response.data.refresh_token;
 
-  // Remove whitespace from chat message
-  const commandName = msg.trim();
-
-  // If the command is known, let's execute it
-  if (commandName === "!d20") {
-    const num = rollDice(commandName);
-    client.say(
-      target,
-      `You rolled a ${num}. Link: https://glitch.com/~twitch-chatbot`
-    );
-    console.log(`* Executed ${commandName} command`);
-  } else if (commandName.startsWith("!add")) {
-    const args = commandName.split(" ");
-    console.log(`what you typed was ${args[1]}`);
-    const res = getAlbum(args[1]);
-    client.say(target, `here's an album you didn't request: ${args[1]}`);
-  } else {
-    console.log(`* Unknown command ${commandName}`);
-  }
-}
-
-// Function called when the "dice" command is issued
-function rollDice() {
-  const sides = 20;
-  return Math.floor(Math.random() * sides) + 1;
-}
-
-// Called every time the bot connects to Twitch chat
-function onConnectedHandler(addr, port) {
-  console.log(`* Connected to ${addr}:${port}`);
-}
-
-const SpotifyWebApi = require("spotify-web-api-node");
-
-// credentials are optional
-let spotifyApi = new SpotifyWebApi({
-  clientId: process.env.SPOT_ID,
-  clientSecret: process.env.SPOT_SECRET,
-  redirectUri: "https://www.mart.pizza",
+      axios({
+        method: "get",
+        url: "https://api.spotify.com/v1/me",
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      }).then((me_response) => {
+        console.log(me_response);
+        res.send(me_response.data);
+      });
+    })
+    .catch((e) => {
+      console.log(e);
+      res.send(e);
+    });
 });
 
-const getAlbum = (album_id) => {
-  // Get album
-  return spotifyApi.getAlbum("5U4W9E5WsYb2jUQWePT8Xm").then(
-    function(data) {
-      console.log("Album information", data.body);
-      return data.body;
-    },
-    function(err) {
-      console.error(err);
-    }
+app.get("/login", (req, res) => {
+  res.redirect(
+    "https://accounts.spotify.com/authorize?" +
+      querystring.stringify({
+        client_id: client_id,
+        response_type: "code",
+        redirect_uri: redirect_uri,
+      })
   );
-};
+});
 
-// authenticate w/ spotify
-// listen on twitrch
-
-// cmd, (song = input_str.split(" "));
-// if (cmd == "!add") {
-//   // add {song} to spotify queue
-// }
+app.listen(port, () => {
+  console.log("listening...");
+});
